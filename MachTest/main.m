0d3811e5fa3f5440c5b63582dce69b9bcd9a55f6
tongleiming <tongleiming1989@sina.com>
2020-02-26 23:16:04 +0800

    Initial Commit

---
+++ MachTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MachTest
+//
+//  Created by tom555cat on 2020/2/26.
+//  Copyright © 2020年 Hello World Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
